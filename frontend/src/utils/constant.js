a332fac20dfb784f92525994fc87f74c39da5d1a
Srikaludev <srikarchaturvedula@gmail.com>
2024-12-10 11:30:22 +0530

    url change for deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:3000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company";
-export const BLOG_API_END_POINT="http://localhost:3000/api/v1/blog";
+export const USER_API_END_POINT="https://cubicals-deploy-backend.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://cubicals-deploy-backend.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://cubicals-deploy-backend.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://cubicals-deploy-backend.onrender.com/api/v1/company";
+export const BLOG_API_END_POINT="https://cubicals-deploy-backend.onrender.com/api/v1/blog";
