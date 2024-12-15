70eec54b81fbe7bf668982e59ae2ad6213b02291
Srikaludev <srikarchaturvedula@gmail.com>
2024-12-15 12:38:57 +0530

    cors added for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,34 +7,39 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import blogRoutes from "./routes/blog.route.js"
-dotenv.config({});
+import blogRoutes from "./routes/blog.route.js";
+
+dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+connectDB();
 
-// api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-app.use("/api/v1/blog" ,blogRoutes)
+app.use("/api/v1/blog", blogRoutes);
 
+app.get("/", (req, res) => {
+    res.send("Cubicles Backend Server is Running");
+});
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT} or on Render`);
+});
